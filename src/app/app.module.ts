33e02be351b98a38dadee25680b32ce771977a89
DIGIMAT <36770624+DIGIMATWEB@users.noreply.github.com>
2024-08-30 11:41:14 -0600

    add files

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './modules/home/pages/home-page/home-page.component';
+import { SharedModule } from "./shared/shared.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
+
+
+
+@NgModule({
+  declarations: [
+    SideBarComponent,
+    HeaderUserComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    SideBarComponent
+  ]
+})
+export class SharedModule { }
