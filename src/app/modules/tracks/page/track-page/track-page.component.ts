65838a131ec84c9f3dd3d65f319e546d95d46df9
DIGIMAT <36770624+DIGIMATWEB@users.noreply.github.com>
2024-09-02 19:48:31 -0600

    update view
    player update

--- src/app/modules/tracks/page/track-page/track-page.component.ts
+++ src/app/modules/tracks/page/track-page/track-page.component.ts
@@ -1,24 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as dataRaw from '../../../../data/tracks.json'
+import { TrackModel } from '../../../../core/models/tracks.model';
 
 @Component({
   selector: 'app-track-page',
   templateUrl: './track-page.component.html',
   styleUrl: './track-page.component.css'
 })
-export class TrackPageComponent {
-  mockTrakcList = [
-  {
-    name: 'BEBE (oficial)'
-  },
-  {
-    name: 'BEBE (oficial)'
-  },
-  {
-    name: 'BEBE (oficial)'
-  }
-
-
-
+export class TrackPageComponent implements OnInit{
+  mockTrakcList: Array<TrackModel> = [
   ]
-
+   ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default
+    this.mockTrakcList=data;
+     console.log(data.data)
+   }
 }
